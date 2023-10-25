bab81ba0252e398817c03fe8fdd868ce6802b454
Jakub Wasik <144064944+wasikjakub@users.noreply.github.com>
2023-10-25 21:22:25 +0200

    code_cleaning

--- firstStrategy.py
+++ firstStrategy.py
@@ -2,53 +2,64 @@ import websocket
 import json
 import pandas as pd
 
-def ConnectToStream():
-    return json.dumps({
-                        "method": "SUBSCRIBE",
-                        "params": ['ethusdt@kline_1m',
-                                'ethusdt@kline_15m'],
-                                "id":1})
+def trading_bot(usd):
 
-def SimulationTrade(msg, endpoint, usd, open_position):
-    returns = {'1m': 0, '15m':0}
+    endpoint = 'wss://stream.binance.com:9443/ws'
 
-    def on_open(ws): 
+    msg = json.dumps({
+        "method": "SUBSCRIBE",
+        "params": ['ethusdt@kline_1m', 'ethusdt@kline_15m'],
+        "id": 1
+    })
+
+    returns = {'1m': 0, '15m': 0}
+    open_position = False
+    printing = True
+    printing2 = True
+    buy_price = 0
+
+    def on_open(ws):
         ws.send(msg)
 
     def on_message(ws, message):
-        global df, open_position, buy_price, usd, loss_counter, profit_counter
+        nonlocal usd, open_position, printing, printing2, buy_price
         out = json.loads(message)
-        df = pd.DataFrame(out['k'],index=[pd.to_datetime(out['E'], unit='ms')])[['s','i','o','c']]
-        df['ret ' + df.i.values[0]] = float(df.c)/float(df.o) - 1
-        returns[df.i.values[0]] = float(df.c)/float(df.o) - 1
-        print(df)
+        df = pd.DataFrame(out['k'], index=[pd.to_datetime(out['E'], unit='ms')])[['s', 'i', 'o', 'c']]
+        df['ret ' + df.i.values[0]] = float(df.c) / float(df.o) - 1
+        returns[df.i.values[0]] = float(df.c) / float(df.o) - 1
+        if printing:
+            print('Waiting for buying condition to be fulfilled ...')
+            printing = False
         if not open_position and returns['15m'] < 0 and returns['1m'] > 0:
             buy_price = float(df.c)
-            open_position = True 
+            open_position = True
             print('bought for ' + str(buy_price))
         if open_position:
-            print('target profit: ' + str(buy_price * 1.002))
-            print('stop loss: ' + str(buy_price * 0.998))
-            print('USD: ' + str(usd))
+            if printing2:
+                print('target profit: ' + str(round(buy_price * 1.002, 2)))
+                print('stop loss: ' + str(round(buy_price * 0.998, 2))) 
+                print('USD: ' + str(usd))
+                print('current price: ' + str(float(df.c)))
+                printing2 = False
             if float(df.c) > buy_price * 1.002:
-                print('SOLD! profit made: ' + str(float(df.c) - buy_price))
+                print('SOLD! profit made: ' + str(round((float(df.c) - buy_price), 2)))
                 open_position = False
+                printing = True
+                printing2 = True
                 usd = usd * (((float(df.c) - buy_price) / 100) + 1)
-                profit_counter += 1
-                print('profits: ' + profit_counter)
+                print('USD: ' + str(usd))
+                print('--------------------')
             elif float(df.c) < buy_price * 0.998:
-                print('LOSS STOPPED! loss: ' + str(float(df.c) - buy_price))
+                print('LOSS STOPPED! loss: ' + str(round((float(df.c) - buy_price), 2)))
                 open_position = False
+                printing = True
+                printing2 = True
                 usd = usd * (((float(df.c) - buy_price) / 100) + 1)
-                loss_counter += 1
-                print('losses: ' +loss_counter)
+                print('USD: ' + str(usd))
+                print('--------------------')
 
     ws = websocket.WebSocketApp(endpoint, on_message=on_message, on_open=on_open)
-
-    ws.run_forever()
-
-
-# msg = ConnectToStream()
-# endpoint = 'wss://stream.binance.com:9443/ws' 
-# open_position=False
-# SimulationTrade(msg, endpoint, usd=1000,open_position=False)
+    try:
+        ws.run_forever()
+    except Exception as error:
+        print('error')
